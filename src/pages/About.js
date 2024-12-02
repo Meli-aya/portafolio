1f3600da17b25a2113eefdaaad9418f8621254ed
Melissa Solano <meli19ayala@gmail.com>
2024-12-01 21:05:14 -0600

    Subiendo archivos del proyecto

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="about">
+            <h2>Sobre Mí</h2>
+            <p>
+                Soy Ingeniera Industrial, me encanta aprendender. Tengo experiencia en el área de Recursos Humanos.
+            </p>
+            <p>
+                Actualmente estoy estudiando un Técnico de Desarrollo de Software, me interesa aprender nuevas tecnologías; además, estoy realizando algunos cursos de la alianza de ESIT con Google.
+            </p>
+            <p>
+                Me interesa especializarme en desarrollo de aplicaciones y en Ciberseguridad.
+            </p>
+        </section>
+    );
+}
+
+export default About;
